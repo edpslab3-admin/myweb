1b73f833532522fd6a1107e6d12707c625f68ff4
EDP <edp.slab3@gmail.com>
2025-11-30 14:20:34 +0700

    Add files via upload
    Signed-off-by: EDP <edp.slab3@gmail.com>

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 10000;
 
-app.use(express.static(path.join(__dirname)));   // ให้เสิร์ฟไฟล์ทั้งหมดในโฟลเดอร์นี้
+app.use(express.static(path.join(__dirname)));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname,'index.html'));
 });
 
-// Render ให้ PORT อัตโนมัติ
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, ()=> console.log('Server running on port '+PORT));
